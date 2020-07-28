e727d612f51dfaec5f148e228870e665414b4fcf
WallaceGomes <wallacecardosogomes@gmail.com>
2020-07-28 10:20:47 -0300

    chore(routes): geting users from database

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
+
+routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
 module.exports= routes;
